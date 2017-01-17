7bd8b7471f70eb9028ad0c088f8b6b17280fc45a
bkimz <writetobriankim@gmail.com>
2017-01-17 11:28:42 -0500

    dao implemented

--- app/src/main/java/com/example/bk/informationorganizer/CompanyDao.java
+++ app/src/main/java/com/example/bk/informationorganizer/CompanyDao.java
@@ -1,8 +1,20 @@
 package com.example.bk.informationorganizer;
 
+import java.util.List;
+
 /**
  * Created by bk on 1/12/2017.
  */
 
-public class CompanyDao {
+public interface CompanyDao {
+
+   void createCompany(String name, String stock, String url);
+   void deleteCompany();
+   void editCompany();
+    List <CompanyModel> getAllCompanies();
+
+
+
+
+
 }
--- app/src/main/java/com/example/bk/informationorganizer/CompanyDaoImp.java
+++ app/src/main/java/com/example/bk/informationorganizer/CompanyDaoImp.java
@@ -1,8 +1,57 @@
 package com.example.bk.informationorganizer;
 
+import java.util.List;
+
 /**
  * Created by bk on 1/12/2017.
  */
 
-public class CompanyDaoImp {
+public class CompanyDaoImp implements CompanyDao {
+    public static List<CompanyModel> testList;
+
+
+    public static CompanyDaoImp onlyCompanyDaoInstance = new CompanyDaoImp();
+
+    private CompanyDaoImp(){
+
+
+
+    }
+
+    public static CompanyDaoImp getCompanyDaoInstance(){
+
+        return onlyCompanyDaoInstance;
+
+    }
+
+    public void createCompany(String name, String stock, String url){
+        CompanyModel test1 = new CompanyModel(name, stock, url);
+        testList.add(test1);
+
+
+    }
+
+
+
+    @Override
+    public void addCompany(CompanyModel companyModel) {
+
+
+
+    }
+
+    @Override
+    public void deleteCompany() {
+
+    }
+
+    @Override
+    public void editCompany() {
+
+    }
+
+    @Override
+    public List<CompanyModel> getAllCompanies() {
+        return null;
+    }
 }
